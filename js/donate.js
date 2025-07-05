f07fc4bfb6c3066406c6dade68d4e5fac8b21b95
Benjamin Mingst <benjaminmingst@outlook.com>
2025-07-05 14:53:04 -0400

    Distance updates

--- js/donate.js
+++ js/donate.js
@@ -7,7 +7,7 @@
 const DONATE_CONFIG = {
   miles: {
     goal: 500,
-    completed: 170.01, // Easy to change - update this value to reflect current progress
+    completed: 207.94, // Easy to change - update this value to reflect current progress
   },
   fundraising: {
     totalRaised: 175, // Easy to change - update this value to reflect current donations
